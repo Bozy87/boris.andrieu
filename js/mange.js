3deb8c86beb6688aab83a98c0f584499ae33d472
robi <bozy.boite@gmail.com>
2023-03-05 21:26:48 +0100

    MAL et rajout Bootstrap

--- js/mange.js
+++ js/mange.js
@@ -334,30 +334,35 @@ if(convert === essai){
 /*----------- PRISE DE CONTACT -----------*/
 
 function clearDisplay() {
-    ecran.innerHTML = "";
+    sorry.innerHTML = "";
 };
+
 
 
 
 const val = document.getElementById('insTel');
 var nbr = document.getElementById('nbrTel');
-var sorry = document.getElementById('sorry');
-const rep = document.getElementById('reponse');
+
+const rep = document.getElementById('reponseembauche');
   let i = "";
 let n = 3;
-  const span = document.createElement("span");
+
 
 val.addEventListener('click',function moins(){
-
+const sorry = document.getElementById('sorry2')
 n--;
   nbr.textContent = n ;
   if(n === 0){
     n = 1;
 
-
+    sorry.innerHTML = "Desolé, plus de disponibilité !";
+rep.parentNode.replaceChild(sorry, rep);
    
   }
 })
+
+
+   
 
 
 
