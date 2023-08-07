25e475aa56eec62569e3ebbadb30f1c7e5281563
Robi <95742139+Bozy87@users.noreply.github.com>
2023-08-07 22:09:13 +0200

    Add files via upload

--- js/mange copy.js
+++ js/mange copy.js
@@ -1,8 +1,29 @@
 
-const tile = document.querySelector(".tile");
+window.addEventListener('scroll', function() {
+  console.log( window.scrollY + 'px');
+});
+
+const m1= document.getElementById("m1");
+const m2 = document.getElementById("m2");
+
+function onWindowScroll(event) {
+	if (window.scrollY < 2341 ) {
+  	m1.style.backgroundColor="#00acf5f3";
+      m2.style.backgroundColor="white";
+  
+  }else if (window.scrollY > 2341  ){
+  	m1.style.backgroundColor="white";
+    m2.style.backgroundColor="#00acf5f3";
+}};
+
+  window.addEventListener('scroll', onWindowScroll);
+
 
 
 /*-------*/
+
+const tile = document.querySelector(".tile");
+
 tile.addEventListener("mouseenter", function () {
   tile.classList.toggle("is-flipped");
 });
