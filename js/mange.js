0b0f1d246d3f857093a9086926fdd5d68621e4cb
Robi <95742139+Bozy87@users.noreply.github.com>
2023-09-03 17:24:38 +0200

    Add files via upload

--- js/mange.js
+++ js/mange.js
@@ -8,16 +8,17 @@ const m2 = document.getElementById("m2");
 
 function onWindowScroll(event) {
 	if (window.scrollY < 2341 ) {
-  	m1.style.backgroundColor="#00a1f0";
-    m1.style.border="solid black";
+  	m1.style.backgroundColor="#f9ee7795";
+    
+    m1.style.border="solid #00a1f0";
       m2.style.backgroundColor="white";
       m2.style.border="none";
   
   }else if (window.scrollY > 2341  ){
   	m1.style.backgroundColor="white";
     m1.style.border="none";
-    m2.style.backgroundColor="#00a1f0";
-    m2.style.border="solid black";
+    m2.style.backgroundColor="#f9ee7795";
+    m2.style.border="solid #00a1f0";
 }};
 
   window.addEventListener('scroll', onWindowScroll);
